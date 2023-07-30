193af5a86176c871e5e7402457c835117bc4bf38
Magenta Paredes <paredesponcemagenta@gmail.com>
2023-07-30 17:08:23 -0500

    Cambios para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: '/src/main.jsx', // Reemplaza '/path/to/your/main.jsx' con la ruta real a tu archivo 'main.jsx'
-    },
-  },
+  base: '/Movie-Challenge-Magenta/',
 })
+
