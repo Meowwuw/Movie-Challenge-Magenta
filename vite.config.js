5cd11267eb27bca44a0f2654c2c1341729f61633
Magenta Paredes <paredesponcemagenta@gmail.com>
2023-07-30 16:48:46 -0500

    PAGES Y VITE

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Movie-Challenge-Magenta/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/src/main.jsx', // Reemplaza '/path/to/your/main.jsx' con la ruta real a tu archivo 'main.jsx'
+    },
+  },
 })
